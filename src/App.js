35e7b7c0f1c446c7b00168b71d675310b1314a08
ryanC <ryan149347@gmail.com>
2024-01-21 22:18:00 +0900

    feat: add widget

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
+import { XSwapWidget } from '@xyfinance/widget'
 import './App.css';
+
+const config = {
+  disabledChains: [],
+  fromInput: '0.1',
+  fromToken: {
+    address: '0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE',
+    chainId: '1'
+  },
+  toToken: {
+    address: '0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE',
+    chainId: '56'
+  },
+  referrer: "0xFD19727868A8197F42e7a52d024374598F62953B",
+  featuredTokens: [
+    {
+      address: '0x666666661f9B6D8c581602AAa2f76cbead06C401',
+      chainId: '56'
+    }
+  ]
+}
+
+const theme = {
+  mode: 'dark',
+  fontFamily: 'proxima-nova',
+  borderRadius: {
+    container: '12px',
+    inner: '8px',
+  },
+  colors: {
+    primary: '#277EEC',
+    gradient: ['#277EEC', '#1499E4']
+  },
+  components: {
+    button: {
+      variant: 'gradient'
+    }
+  }
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div
+        style={{
+          width: '480px'
+        }}
+      >
+        <XSwapWidget config={config} theme={theme} />
+      </div>
     </div>
   );
 }
